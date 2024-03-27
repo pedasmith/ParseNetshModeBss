e07955d47202bce6126e862d91ca4fc494bea8f3
Peter Smith <shipwrecksoftware@live.com>
2024-03-26 19:53:11 -0700

    Remove old using

--- NetshG/CommandOutputControl.xaml.cs
+++ NetshG/CommandOutputControl.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using ParseNetshModeBss;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Utilities;
 using Windows.ApplicationModel.DataTransfer;
 using static NetshG.CanDoCommand;
-using Windows.UI.Popups;
 using Clipboard = Windows.ApplicationModel.DataTransfer.Clipboard;
 
 namespace NetshG
