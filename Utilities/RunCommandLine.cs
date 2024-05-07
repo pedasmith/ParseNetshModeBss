4fb26206649a0d2042dbcd31d8b05684bcd3d93f
Peter Smith <shipwrecksoftware@live.com>
2024-05-07 14:13:56 -0700

    Supports URLs in the Macro list (and the other too, I guess)

--- Utilities/RunCommandLine.cs
+++ Utilities/RunCommandLine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -27,6 +28,56 @@ namespace ParseNetshModeBss
             {
                 retval = proc?.StandardOutput.ReadToEnd() ?? "";
                 proc!.WaitForExit();
+            }
+            return retval;
+        }
+
+        public static async Task<string> RunOpenUrl(string url, AddToText? tb = null)
+        {
+            //Uri? uri;
+            //bool created = Uri.TryCreate(url, UriKind.Absolute, out uri);
+            //await Task.Delay(0);
+            string retval = "";
+            var start = new ProcessStartInfo (url) {  UseShellExecute = true };
+
+            int mod = 1;
+            int nline = 0;
+            using (Process? proc = Process.Start(start))
+            {
+                if (proc != null)
+                {
+                    string? line;
+                    do
+                    {
+                        line = await proc.StandardOutput.ReadLineAsync();
+                        retval += line + "\n";
+
+                        // Have to batch the lines. Otherwise commands with lots of output like
+                        // netsh advfirewall firewall show rule name=all
+                        // will cause the UX to stall.
+                        if (nline % mod == 0)
+                        {
+                            if (tb != null) tb.DoAddToText(line + "\n");
+                        }
+                        switch (nline)
+                        {
+                            case 50: mod = 2; break;
+                            case 100: mod = 10; break;
+                            case 500: mod = 100; break;
+                        }
+                        nline++;
+                    }
+                    while (line != null);
+                    // retval = await proc.StandardOutput.ReadToEndAsync();
+                    // FYI: it seems like there's no standard error to read. It's in the object,
+                    // but just looking at it throws an exception.
+
+                    await proc.WaitForExitAsync();
+                }
+                else
+                {
+                    retval = "";
+                }
             }
             return retval;
         }
